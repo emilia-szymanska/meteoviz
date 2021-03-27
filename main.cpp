5c8b62b93ce1d48c5f566c1dca19d94e0c04041e
emilia-szymanska <emiliaszym@gmail.com>
2021-03-27 23:05:24 +0100

    Change default qml file launch

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/citychoice.qml"));
+    //const QUrl url(QStringLiteral("qrc:/citychoice.qml"));
+    const QUrl url(QStringLiteral("qrc:/weather.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
